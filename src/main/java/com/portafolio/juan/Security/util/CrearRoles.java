f93f811b3a9e7c52885694837f61d1f83cfb98cb
JuanLuna21 <xjuanigancio@gmail.com>
2022-10-21 00:15:13 -0300

    cambio de base de datos

---
+++ src/main/java/com/portafolio/juan/Security/util/CrearRoles.java
@@ -0,0 +1,24 @@
+
+package com.portafolio.juan.Security.util;
+
+import com.portafolio.juan.Security.Entity.Rol;
+import com.portafolio.juan.Security.Enums.RolNombre;
+import com.portafolio.juan.Security.Service.RolService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+/*
+    @Component
+public class CrearRoles implements CommandLineRunner {
+    @Autowired
+    RolService rolService;
+   @Override
+   public void run(String... args) throws Exception {
+       Rol rolAdmin = new Rol(RolNombre.ROLE_ADMIN);
+        Rol rolUser = new Rol(RolNombre.ROLE_USER);
+       rolService.save(rolAdmin);
+       rolService.save(rolUser);
+   }
+}
+*/
+
